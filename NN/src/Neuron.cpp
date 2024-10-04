d3d0b4a2532b952c031a14d2eb39b63a50416caf
sghatak5 <sagnikghatak22@gmail.com>
2024-10-05 00:45:44 +0200

    Add Neuron.cpp

---
+++ NN/src/Neuron.cpp
@@ -0,0 +1,19 @@
+#include "../include/Neuron.hpp"
+#include <cmath>
+
+//Constructor
+Neuron::Neuron(double val) {
+    this->val = val;
+    activate();
+    derive();
+}
+
+// activation function f(x) = x / (1 + |x|)
+void Neuron::activate() {
+    this->activatedVal = this->val / (1 + abs(this->val));
+}
+
+// f'(x) = f(x) * (1 - f(x))
+void Neuron::derive() {
+    this->derivedVal = this->activatedVal * (1 - this->activatedVal);
+}
--- NN/src/main.cpp
+++ NN/src/main.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
+#include "../include/Neuron.hpp"
 
 using namespace std;
 
 int main(int argc, char **argv) {
-    
+    Neuron *n = new Neuron(0.9);
+    cout << "Vall: " << n->getVal() << endl;
+    cout << "Activated Vall: " << n->getActivatedVal() << endl;
+    cout << "Derived Vall: " << n->getDerivedVal() << endl;
     // cout << "Hello, I am here ";
     return 0;
 }
